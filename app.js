6a750692faefa4477268b3def7f3c67a0354487d
wonedd <vinmarv@hotmail.com>
2023-07-18 22:47:47 -0300

    donwload working

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ import express from 'express'
 import axios from 'axios'
 import dotenv from 'dotenv';
 import ejs from 'ejs';
+import ytdl from 'ytdl-core';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg';
+import ffmpeg from 'fluent-ffmpeg';
+
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 dotenv.config();
 
@@ -20,95 +27,75 @@ app.get('/', (req, res) => {
 
 
 app.post('/convert', async (req, res) => {
-  const { videos } = await req.body;
-  // const result = await getId(videos);
-  return res.json({
-    videos
-  });
+  const value = req.body;
 
+  const audioFileName = 'output';
+  const convertedVideos = []; // Array para armazenar os nomes dos vídeos convertidos
 
+  async function fetchVideo(url, audioFileName) {
+    return new Promise((resolve, reject) => {
+      const videoStream = ytdl(url, { quality: 'highestaudio' });
 
-  if (!videoId) {
-    return res.json({
-      error: "invalid value"
-    })
+      const ffmpegCommand = ffmpeg(videoStream)
+        .audioBitrate(128)
+        .toFormat('mp3')
+        .save(audioFileName)
+        .on('end', () => {
+          convertedVideos.push(audioFileName); // Adiciona o nome do vídeo convertido ao array
+          resolve();
+        })
+        .on('error', (err) => {
+          reject(err);
+        });
+
+      ffmpegCommand.run();
+    });
   }
 
-  const options = {
-    method: 'GET',
-    url: 'https://youtube-mp36.p.rapidapi.com/dl',
-    params: { id: videoId },
-    headers: {
-      'X-RapidAPI-Key': process.env.API_KEY,
-      'X-RapidAPI-Host': process.env.API_HOST
-    }
-  };
-
-  try {
-    const response = await axios.request(options);
-
-    const result = {
-      success: true,
-      song_title: response.data.title,
-      song_link: response.data.link
-    };
-
-    return res.json(result);
-
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: error.message,
+  if (value.videos.length > 0) {
+    const promises = value.videos.map((v, index) => {
+      const audioFileNameWithIndex = `${audioFileName}_${index}.mp3`;
+      return fetchVideo(v, audioFileNameWithIndex)
+        .then(() => {
+          console.log('Done');
+        })
+        .catch((err) => {
+          console.log('Error', err);
+        });
     });
+
+    Promise.all(promises)
+      .then(() => {
+        // Todos os vídeos foram convertidos
+        return res.json({ success: true, videos: convertedVideos }); // Envia o array de nomes dos vídeos convertidos para o front-end
+      })
+      .catch((err) => {
+        console.log('Error', err);
+        return res.status(500).json({ error: 'Error converting videos' });
+      });
+  } else {
+    return res.status(400).json({ error: 'No videos provided' });
   }
 });
 
+app.get('/download/:index', (req, res) => {
+  const index = req.params.index;
+  const audioFileNameWithIndex = `output_${index}.mp3`;
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, audioFileNameWithIndex);
 
-const getId = async (links) => {
-  try {
-    if (typeof links === 'string') {
-      let videoId = "";
-      if (links.includes("youtube.com") || links.includes("youtu.be")) {
-        const videoIdMatch = links.match(/[?&]v=([^&#]*)/);
-        videoId = videoIdMatch && videoIdMatch[1];
-      } else {
-        throw new Error(`Invalid YouTube link: ${links}`);
-      }
-
-      if (!videoId) {
-        throw new Error(`Video ID not found in link: ${links}`);
-      }
-
-      return [videoId];
-    } else if (Array.isArray(links)) {
-      const videoIds = [];
-
-      for (const fullLink of links) {
-        let videoId = "";
-
-        if (fullLink.includes("youtube.com") || fullLink.includes("youtu.be")) {
-          const videoIdMatch = fullLink.match(/[?&]v=([^&#]*)/);
-          videoId = videoIdMatch && videoIdMatch[1];
-        } else {
-          throw new Error(`Invalid YouTube link: ${fullLink}`);
-        }
-
-        if (!videoId) {
-          throw new Error(`Video ID not found in link: ${fullLink}`);
-        }
-
-        videoIds.push(videoId);
-      }
-
-      return videoIds;
+  res.download(filePath, (err) => {
+    if (err) {
+      console.error('Error downloading file:', err);
+      // Trate o erro de acordo com as suas necessidades
+      res.status(500).json({ error: 'Error downloading file' });
     } else {
-      throw new Error("Invalid argument: links must be a string or an array");
+      // O download foi concluído com sucesso
     }
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
+  });
+});
+
 
 
 app.listen(PORT, () => {
